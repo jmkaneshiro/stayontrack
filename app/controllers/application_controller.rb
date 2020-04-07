9a3dd3a1a47df969a7ce0fcecf661b9285d0189a
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-07 08:26:20 -0400

    Removes code comment from the application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
 
-  CELLL
-
   def current_user
     return nil unless session[:session_token]
     @current_user ||= User.findBy(session_token: session[:session_token])
