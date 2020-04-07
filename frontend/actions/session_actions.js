74299cfe791cd0fabdc7e3bdd027448d18ae9dc9
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-07 16:19:11 -0400

    Fixes two naming issues in session actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,8 +1,8 @@
 import * as SessionApiUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
-export const LOGOUT_CURRENT_USER = "RECEIVE_CURRENT_USER";
-export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 
 const receiveCurrentUser = (currentUser) => ({
   type: RECEIVE_CURRENT_USER,
@@ -14,7 +14,7 @@ const logoutCurrentUser = () => ({
 });
 
 const receiveSessionErrors = (errors) => ({
-  type: RECEIVE_ERRORS,
+  type: RECEIVE_SESSION_ERRORS,
   errors
 });
 
