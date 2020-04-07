a33a3c96fa63f21ef07c2105e54735ce41fef1d0
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-07 14:43:57 -0400

    Adds errors reducer and fixes naming issue on entities reducer

---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import sessionErrorsReducer from './sessionErrorsReducer';
+
+const errorsReducer = combineReducers({
+  session: sessionErrorsReducer
+});
+
+export default errorsReducer;
