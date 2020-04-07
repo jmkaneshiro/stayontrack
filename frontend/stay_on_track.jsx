3b3081560df31dfb09beea7de938d84346f83bc6
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-07 15:08:31 -0400

    Adds store to entry file and adds testing notes

--- frontend/stay_on_track.jsx
+++ frontend/stay_on_track.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import * as SessionApiUtil from "./util/session_api_util";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
   // TESTING
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.signup = SessionApiUtil.signup;
   window.login = SessionApiUtil.login;
   window.logout = SessionApiUtil.logout;
