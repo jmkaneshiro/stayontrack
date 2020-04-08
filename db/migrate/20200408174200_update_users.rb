b1d3313850204408e228c188423341b42acddd86
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-08 13:52:31 -0400

    Removed columns, unnecessary for signup

---
+++ db/migrate/20200408174200_update_users.rb
@@ -0,0 +1,7 @@
+class UpdateUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :firstname
+    remove_column :users, :lastname
+    remove_column :users, :username
+  end
+end
