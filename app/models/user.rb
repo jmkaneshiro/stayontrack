8f14acbc6be29f2db01facdba11eb2e2e6aca67e
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-08 13:54:38 -0400

    Removes username, firstname, lastname validations from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,15 @@
 # Table name: users
 #
 #  id              :bigint           not null, primary key
-#  username        :string           not null
 #  email           :string           not null
-#  firstname       :string           not null
-#  lastname        :string           not null
 #  password_digest :string           not null
 #  session_token   :string           not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-  validates :username, :email, :session_token, presence: true, uniqueness: true
-  validates :firstname, :lastname, :password_digest, presence: true
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
   validates :password, length: {minimum: 6}, allow_nil: true
   attr_reader :password
 
