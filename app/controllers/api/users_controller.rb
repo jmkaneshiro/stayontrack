7271d48fa20302daf4d70ff9aa8a2169dbb752ff
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-08 13:55:41 -0400

    Removes username, firstname, lastname from user_params on users_controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,6 +11,6 @@ class Api::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :username, :firstname, :lastname)
+    params.require(:user).permit(:email, :password)
   end
 end
