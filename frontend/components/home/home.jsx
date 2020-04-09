2229fd1be49537094537ce40f077844085cebb1a
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-09 08:06:57 -0400

    Seaparates styling concerns into multiple files

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,74 +3,93 @@ import { Link } from "react-router-dom";
 
 const Home = ({ currentUser }) => (
   <>
-    <nav className="header">
+    <nav className="main-nav">
       <div className="page-container">
-        <div className="header-subcontainer">
+        <div className="main-nav-container">
           <Link to="/">
             <img src={window.logoURL} alt="Logo" className="logo" />
           </Link>
           <div className="header-links">
-            <a className="header-link header-info">Product</a>
-            <a className="header-link header-info">Community</a>
-            <a href="https://www.pivotaltracker.com/blog" className="header-link header-info">Blog</a>
-            <Link id="sign-in-link" className="header-link" to="/signin">Sign In</Link>
-            <Link id="sign-up-link" className="header-link" to="/signup">Sign Up</Link>
+            <a className="header-link">Product</a>
+            <a className="header-link">Community</a>
+            <a href="https://www.pivotaltracker.com/blog" className="header-link">Blog</a>
+            <Link id="sign-in-link" className="header-link btn btn-orange" to="/signin">Sign In</Link>
+            <Link id="sign-up-link" className="header-link btn btn-blue" to="/signup">Sign Up</Link>
           </div>
         </div>
       </div>
-      
     </nav>
     <div className="home-container">
       <section className="hero">
-        <h1>
-          StayOnTrack is changing how teams build software-- one story at a time
-        </h1>
-        <div className="work-in-progress">
-          Work in progress ...
+        <div className="text-box">
+          <h1 className="hero-heading">
+            StayOnTrack is changing how teams build software-- one story at a time
+          </h1>
+          <div className="work-in-progress">
+            Work in progress ...
+          </div>
         </div>
       </section>
       <section className="section-md">
-        <div>
-          <h3>Better organization to get focused</h3>
-          <p>
-            <strong>Keep your team on the rails.</strong>Tracker's shared backlog makes priorities clear
-            so the team can stay organized. Easily visualize scope, focus your teamwork, 
-            and stay nimble when circumstances change.
-          </p>
+        <div className="page-container">
+          <h2 className="banner-msg">
+            Proven project management for successful teams
+          </h2>
+          <h3 className="banner-msg">
+            With a shared view of team priorities, a process that fosters collaboration, and dynamic 
+            tools to analyze progress, your team will deliver more frequently and consistently.
+          </h3>
         </div>
       </section>
       <section className="section-md">
-        <div>
-          <h3>Tools to help you adapt and evolve</h3>
-          <p>
-            <strong>Get more work done, more often.</strong>Tracker's guided iteration planning helps you
-          break down and prioritize projects into manageable chunks so the team can keep the momentum
-          toward delivering.
-        </p>
-        </div>
-
-      </section>
-      <section className="section-md">
-        <div>
-          <h3>Team transparency at a glance</h3>
-          <p>
-            <strong>No more surprises.</strong>With a shared, clear view of your team's work, everyone has a
-            real-time, single source of truth. A quick scan explains your team's status, who's responsible
-            for what, and what's coming next.
-          </p>
+        <div className="page-container">
+          <div className="info">
+            <div className="info-text">
+              <h3>Better organization to get focused</h3>
+              <p>
+                <strong>Keep your team on the rails.</strong>Tracker's shared backlog makes priorities clear
+                so the team can stay organized. Easily visualize scope, focus your teamwork
+                and stay nimble when circumstances change.
+              </p>
+            </div>
+            <img src={window.workingTogether1URL} alt="Illustration showing how StayOnTrack iterations 
+            provide for better organization for agile software teams to get focused" className="info-image"/>
+          </div>
         </div>
       </section>
       <section className="section-md">
-        <div>
-          <h3>Better organization to get focused</h3>
-          <p>
-            <strong>Keep your team on the rails.</strong>Tracker's shared backlog makes priorities clear
-            so the team can stay organized. Easily visualize scope, focus your teamwork,
-            and stay nimble when circumstances change.
-          </p>
+        <div className="page-container">
+          <div className="info">
+            <img src={window.workingTogether2URL} alt="Illustration showing how Pivotal Tracker has product 
+            management tools to help agile software teams adapt and evolve" className="info-image" />
+            <div className="info-text">
+              <h3>Tools to help you adapt and evolve</h3>
+              <p>
+                <strong>Get more work done, more often.</strong>Tracker's guided iteration planning helps you
+            break down and prioritize projects into manageable chunks so the team can keep the momentum
+            toward delivering.
+            </p>
+            </div>
+          </div>
         </div>
       </section>
       <section className="section-md">
+        <div className="page-container">
+          <div className="info">
+            <div className="info-text">
+              <h3>Team transparency at a glance</h3>
+              <p>
+                <strong>No more surprises.</strong>With a shared, clear view of your team's work, everyone has a
+              real-time, single source of truth. A quick scan explains your team's status, who's responsible
+              for what, and what's coming next.
+            </p>
+            </div>
+            <img src={window.workingTogether3URL} alt="Illustration showing how StayOnTrack provides agile 
+            software teams transparency at a glance for easier collaboration" className="info-image" />
+          </div>
+        </div>
+      </section>
+      {/* <section className="section-md">
         <div>
           <h2>A better way to develop</h2>
           <p>
@@ -85,8 +104,8 @@ const Home = ({ currentUser }) => (
         <a className="signup-block-btn">Get started!</a>
       </section>
       <section className="section-md">
-        {/* computer image goes here */}
-      </section>
+        computer image goes here
+      </section> */}
     </div>
   </>
 
