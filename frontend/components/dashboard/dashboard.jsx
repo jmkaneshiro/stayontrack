44660e86cb38d45a12192f9c6d4269e98eb566f3
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-14 11:32:50 -0400

    Adds skeleton for dashboard

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 class Dashboard extends React.Component {
   constructor (props) {
@@ -10,8 +11,70 @@ class Dashboard extends React.Component {
 
     return (
       <>
-        <h1 className="under-construction">Hello World!</h1>
-        < button className = "btn btn-blue" onClick = { logout } > Log Out </button>
+        <header className="dashboard-header">
+          <nav className="project-nav header-toggle-dropdown">
+            <Link to="/">
+              <img src={window.logoWhiteURL} alt="Logo" className="logo" />
+            </Link>
+            <div className="arrow-down"></div>
+          </nav>
+          <div className="user-nav">
+            <div className="notifications header-toggle-dropdown">
+              <i className="fas fa-bell"></i>
+            </div>
+            <div className="header-toggle-dropdown">
+              WHAT'S NEW <div className="arrow-down"></div>
+            </div>
+            <div className="header-toggle-dropdown">
+              HELP <div className="arrow-down"></div>
+            </div>
+            <nav className="user-profile header-toggle-dropdown">
+              {this.props.currentUser.username} <div className="arrow-down"></div>
+            </nav>
+          </div>
+        </header>
+        <section className="dashboard">
+          <div className="dashboard-tabs-container">
+            <div className="dashboard-tabs-wrapper">
+              <div className="dashboard-tabs">
+                <div className="dashboard-tab active-tab">Projects</div>
+                <div className="dashboard-tab inactive-tab">Workspaces</div>
+              </div>
+              <button className="dashboard-action-tabs-btn btn btn-green">
+                Create project
+              </button>
+            </div>
+          </div>
+          <div className="projects-pane-container">
+            <section className="projects-pane">
+              <section className="projects-top-menu">
+                <div className="projects-searchbar">
+                  <i className="fas fa-search"></i>
+                  <input placeholder="Search Active Projects" className="searchbar-input"></input>
+                </div>
+                <div className="projects-quick-dropdown">
+                  <span>Active Projects</span>
+                  <i className="fas fa-chevron-down"></i>
+                </div>
+              </section>
+              <section className="my-projects">
+                <h1>
+                  <i class="fas fa-bars"></i> <span>My Projects |</span>
+                  <span className="project-count">1</span>
+                </h1>
+                <ul className="project-list">
+                  <li>
+                    <section className="project-container">
+                      Demo Project
+                    </section>
+                  </li>
+                </ul>
+              </section>
+              <button className="btn btn-blue" onClick={logout} > Log Out </button>
+            </section>
+          </div>
+
+        </section>
       </>
     );
   }
