d3d9754821864b9d7bb5a5b1275fa057a6b8f371
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-17 10:39:08 -0400

    Adds uniqueness constraint on project memberships

--- app/models/project_membership.rb
+++ app/models/project_membership.rb
@@ -9,6 +9,7 @@
 #  updated_at       :datetime         not null
 
 class ProjectMembership < ApplicationRecord
+  validates :member_id, :uniqueness {scope: :project_id }
   belongs_to :member,
     foreign_key: :member_id,
     class_name: :User
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_211036) do
+ActiveRecord::Schema.define(version: 2020_04_17_143524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_211036) do
     t.integer "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["member_id", "project_id"], name: "index_project_memberships_on_member_id_and_project_id"
   end
 
   create_table "projects", force: :cascade do |t|
