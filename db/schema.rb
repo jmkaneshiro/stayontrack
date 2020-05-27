55fd86395ce15e977c56bea58446267a9e0629a7
jmkaneshiro <jmkaneshiro@gmail.com>
2020-05-27 18:54:05 -0400

    Adds stories table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_143524) do
+ActiveRecord::Schema.define(version: 2020_05_27_224706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,18 @@ ActiveRecord::Schema.define(version: 2020_04_17_143524) do
   create_table "projects", force: :cascade do |t|
     t.string "title", null: false
     t.integer "project_owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "type", null: false
+    t.integer "story_owner_id", null: false
+    t.integer "project_id", null: false
+    t.string "story_state", null: false
+    t.integer "story_assignee_id"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
