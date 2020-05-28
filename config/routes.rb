9df0455141022ad16c1c9e707e1b32a817b959d0
jmkaneshiro <jmkaneshiro@gmail.com>
2020-05-27 20:33:37 -0400

    Adds route for stories

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
-    resources :projects, only: [:index, :create, :show]
+    resources :projects, only: [:index, :create, :show] do
+      resources :stories, only: [:create, :show]
+    end
     resources :project_memberships, only: [:create]
     resource :session, only: [:create, :destroy, :show]
   end
