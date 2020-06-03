d0e523ce6e08aa03f9831430ddf0185002b38471
jmkaneshiro <jmkaneshiro@gmail.com>
2020-06-03 16:08:02 -0400

    Hides unused toggles

--- frontend/components/navigation/top_navigation.jsx
+++ frontend/components/navigation/top_navigation.jsx
@@ -28,24 +28,24 @@ class TopNavigation extends React.Component {
             </ul>
           </nav>
           <div className="user-nav">
-            <div className="toggle-dropdown">
+            {/* <div className="toggle-dropdown">
               <input type="checkbox" id="notifications-toggle" />
               <label htmlFor="notifications-toggle" className="toggle-dropdown-label">
                 <i className="fas fa-bell"></i>
               </label>
-            </div>
-            <div className="toggle-dropdown">
+            </div> */}
+            {/* <div className="toggle-dropdown">
               <input type="checkbox" id="stayontrack-updates-toggle" />
               <label htmlFor="stayontrack-updates-toggle" className="toggle-dropdown-label">
                 <div>What's New<span className="arrow-down"></span></div>
               </label>
-            </div>
-            <div className="toggle-dropdown">
+            </div> */}
+            {/* <div className="toggle-dropdown">
               <input type="checkbox" id="help-toggle" />
               <label htmlFor="help-toggle" className="toggle-dropdown-label">
                 <div>Help<span className="arrow-down"></span></div>
               </label>
-            </div>
+            </div> */}
             <nav className="toggle-dropdown user-profile-dropdown">
               <input type="checkbox" id="user-profile-toggle" />
               <label htmlFor="user-profile-toggle" className="toggle-dropdown-label">
